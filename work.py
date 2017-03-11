f58f3d83be7343c468f78cb6b34f46d7333fbacc
computereng <worawuth.kab@siam.edu>
2017-03-11 11:30:45 +0700

    Update work.py

--- work.py
+++ work.py
@@ -1,27 +1,7 @@
 import sys
 def main():
-  for arg in sys.argv[1:]:
-    print("We get file at",arg)
-    filehandle = open(arg, 'r')
-    
-    for line in filehandle:
-    
-      try:
-        try:
-          print(line)
-          text = line
-        expect NameError:
-          print("Except in ", line)
-          text = line
-      except EOFError:
-        print("Except in EOF Error")
-        break
-      if not line:
-        print("Not Line")
-        continue
-      for word in line.split(" "):
-        print("Phase I lexer created with:", word)
-        
+  print("We get file at",arg)
+  
         
         
         
